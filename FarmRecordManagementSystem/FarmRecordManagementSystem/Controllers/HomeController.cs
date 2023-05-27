d70faee23509fa8f340ffeda2bb738f32c1695f9
coxmd <coxmusyoki@gmail.com>
2023-05-27 14:21:17 +0300

    Added the Login View

--- FarmRecordManagementSystem/FarmRecordManagementSystem/Controllers/HomeController.cs
+++ FarmRecordManagementSystem/FarmRecordManagementSystem/Controllers/HomeController.cs
@@ -13,6 +13,11 @@ namespace FarmRecordManagementSystem.Controllers
             _logger = logger;
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
         public IActionResult Index()
         {
             return View();
---
+++ FarmRecordManagementSystem/FarmRecordManagementSystem/Models/AppUsers.cs
@@ -0,0 +1,9 @@
+namespace FarmRecordManagementSystem.Models
+{
+    public class AppUsers
+    {
+        public string? UserName { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
