4cc01ff94eab1a8a238466855a79de04d5fbf6dd
coxmd <coxmusyoki@gmail.com>
2023-05-29 21:48:01 +0300

    Added Crops model

---
+++ FarmRecordManagementSystem/FarmRecordManagementSystem/Models/Crops.cs
@@ -0,0 +1,13 @@
+namespace FarmRecordManagementSystem.Models
+{
+    public class Crops
+    {
+        public int? Id { get; set; }
+
+        public string? Name { get; set; }
+        public string? Variety { get; set; }
+        public int? FarmId { get; set; }
+        public string? ExpectedHarvestDate { get; set; }
+        public DateTime? PlantingDate { get; set; }
+    }
+}
