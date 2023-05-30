a5c5ee40c74aca8ac507f513e479a63652eca52d
coxmd <coxmusyoki@gmail.com>
2023-05-30 19:08:27 +0300

    Added View to create a new Farm

--- FarmRecordManagementSystem/FarmRecordManagementSystem/Models/Land.cs
+++ FarmRecordManagementSystem/FarmRecordManagementSystem/Models/Land.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FarmRecordManagementSystem.Models
 {
     public class Land
     {
         public int? Id { get; set; }
 
+        [Display(Name = "Farm Name")]
         public string? Name { get; set; }
+        [Display(Name = "Farm Size (in hectares)")]
         public int? Size { get; set; }
         public string? SoilType { get; set; }
     }
